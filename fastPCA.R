fb9cec088a3a584d36935ba9824b16113152ac13
haachicanoy <harold22010@gmail.com>
2017-10-04 23:08:22 -0500

    changes

--- fastPCA.R
+++ fastPCA.R
@@ -1,7 +1,6 @@
-pca_svd <- function(x){
+pca_svd <- function(x, modos){
   
   n     <- dim(x)[1]
-  modos <- dim(x)[2]
   x0    <- scale(x) # *(sqrt(n)/sqrt(n-1))
   svd_o <- corpcor::fast.svd(x0)
   comp  <- svd_o$u[,1:modos, drop = F] %*% diag(svd_o$d[1:modos], length(svd_o$d[1:modos]), length(svd_o$d[1:modos])) 
@@ -11,7 +10,7 @@ pca_svd <- function(x){
   
 }
 
-fpca <- pca_svd(x = mtcars)
+fpca <- pca_svd(x = mtcars, modos = 5)
 tpca <- FactoMineR::PCA(X = mtcars, scale.unit = T, ncp = ncol(mtcars), graph = F)
 
 View(tpca$ind$coord)
