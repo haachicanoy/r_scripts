3880ed3e548b55d8242c9ea8c9e21a938ef658b5
haachicanoy <harold22010@gmail.com>
2022-05-10 22:21:41 -0500

    chirps2gray function ready

--- CHIRPS_to_GrayImages.R
+++ CHIRPS_to_GrayImages.R
@@ -18,47 +18,75 @@ ini <- as.Date('1981-01-01')
 end <- as.Date('2021-12-31')
 dts <- seq(from = ini, to = end, by = 'day'); rm(ini, end)
 
-# Download CHIRPS
-chrps <- 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05'
-Day  <- dts[1]
-Year <- lubridate::year(Day)
-file <- paste0(chrps,'/',Year,'/chirps-v2.0.',gsub('-','.',Day,fixed=T),'.tif.gz')
+# Output directory
 Out  <- 'D:/download_test'; if(!dir.exists(Out)){dir.create(Out,F,T)}
-dfile <- paste0(Out,'/',basename(file))
 
-if(!file.exists(dfile)){
-  tryCatch(expr = {
-    utils::download.file(url = file, destfile = dfile)
-  },
-  error = function(e){
-    cat(paste0(basename(file),' failed.\n'))
-  })
+# Main function
+chirps2gray <- function(date = dts[1]){
+  # CHIRPS base URL
+  chrps <- 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05'
+  # Get day and year
+  Day  <- date
+  Year <- lubridate::year(Day)
+  # Target file
+  tfile <- paste0(chrps,'/',Year,'/chirps-v2.0.',gsub('-','.',Day,fixed=T),'.tif.gz')
+  # Destination file
+  dfile <- paste0(Out,'/',basename(tfile))
+  # Raster file
+  rfile <- gsub('.gz','',dfile,fixed = T)
+  # Image file
+  ifile <- gsub('.tif','.png',rfile,fixed = T)
+  
+  if(!file.exists(ifile)){
+    
+    # Downloading
+    if(!file.exists(dfile)){
+      tryCatch(expr = {
+        utils::download.file(url = tfile, destfile = dfile)
+      },
+      error = function(e){
+        cat(paste0(basename(tfile),' failed.\n'))
+      })
+    }
+    
+    # Unzip
+    R.utils::gunzip(dfile)
+    
+    # Identify 512 x 512 pixels region
+    r <- raster::raster(x = rfile)
+    # Centroid
+    x_pos <- raster::colFromX(object = r, x = crd$x)
+    y_pos <- raster::rowFromY(object = r, y = crd$y)
+    # Associated extent
+    ext <- raster::extent(r, y_pos-256, y_pos+255, x_pos-256, x_pos+255)
+    # Cropping the raster
+    r <- raster::crop(x = r, y = ext)
+    # Assigning NAs
+    r[r[] == -9999] <- NA
+    sts <- range(r[], na.rm = T) # Important to use in any moment
+    # Normalizing raster values between 0 and 1
+    r_nrm <- r/max(r[], na.rm = T)
+    
+    # Read auxiliary image
+    aux <- system.file('extdata/parrots.png',package='imager')
+    img <- imager::load.image(aux) %>% imager::resize(512,512) %>% imager::grayscale(); rm(aux)
+    # Assigning raster values to gray image format
+    img[,,1,1] <- raster::as.matrix(r_nrm)
+    
+    # Save the result
+    save.Image <- function(im = img, file = ifile){
+      im <- imager::imrotate(im, 90) %>% imager::mirror("x")
+      dim(im) <- dim(im)[-3]
+      im %>% png::writePNG(file)
+    }
+    save.Image(im = img, file = ifile)
+    file.remove(rfile)
+    return(cat(paste0('Image ',basename(ifile),' processed correctly!!!\n')))
+  } else {
+    return(cat(paste0('Image ',basename(ifile),' already exists!\n')))
+  }
+  
 }
 
-R.utils::gunzip(dfile)
-
-rfile <- gsub('.gz','',dfile,fixed = T)
-r <- raster::raster(x = rfile)
-x_pos <- raster::colFromX(object = r, x = crd$x)
-y_pos <- raster::rowFromY(object = r, y = crd$y)
-# Define 512 x 512 px region
-ext <- raster::extent(r, y_pos-256, y_pos+255, x_pos-256, x_pos+255)
-r <- raster::crop(x = r, y = ext)
-r[r[] == -9999] <- NA
-sts <- range(r[], na.rm = T)
-r_nrm <- r/max(r[], na.rm = T)
-
-
-aux <- system.file('extdata/parrots.png',package='imager')
-img <- imager::load.image(aux) %>% imager::resize(512,512) %>% imager::grayscale(); rm(aux)
-img[,,1,1] <- raster::as.matrix(r_nrm)
-
-ifile <- gsub('.tif','.png',rfile,fixed = T)
-save.Image <- function(im = img, file = ifile){
-  im <- imager::imrotate(im, 90) %>% imager::mirror("x")
-  dim(im) <- dim(im)[-3]
-  im %>% png::writePNG(file)
-}
-save.Image(im = img, file = ifile)
-
-file.remove(rfile)
+# Loop through the dates
+dts %>% purrr::map(.f = chirps2gray)
